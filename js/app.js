840939298574a5034b727cce8b953289976dbc8c
Masumi Hayashi-Smith <masumi.hs@gmail.com>
2015-09-07 20:45:24 -0700

    added click-to-edit button

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
+var SongProperty = React.createClass({
+  render: function() {
+    var editable = this.props.editable;
+
+    if (!this.props.value) {
+      return <div />;
+    }
+
+    return (
+      <div>
+        <span className="song-property-name">{this.props.name}</span>
+        <span className="song-property-divider">: </span>
+        <span className="song-property-value" contentEditable={editable}>{this.props.value}</span>
+      </div>
+    );
+  }
+});
+
 var Song = React.createClass({
+  getInitialState: function() {
+    return {editable: false};
+  },
   render: function() {
     var currentSong = this.props.currentSong;
 
@@ -6,40 +27,44 @@ var Song = React.createClass({
       return (<div/>);
     }
 
-    var createItem = function(key, index) {
-      return (
-        <li>{key}: {currentSong[key]}</li>
-      );
-    };
+    var toggleEditMode = function() {
+      this.setState({editable: !this.state.editable});
+    }.bind(this);
 
     return (
       <div>
+        <button onClick={toggleEditMode}>{this.state.editable ? 'Editing' : 'Click to Edit'}</button>
         <h4>{currentSong.title}</h4>
         <h5>{currentSong.alternateTitles}</h5>
         <h5>analysis</h5>
-        <div>gradeFloor: {currentSong.gradeFloor}</div>
-        <div>gradeCeil: {currentSong.gradeCeil}</div>
-        <div>toneSet: {currentSong.toneSet}</div>
-        <div>range: {currentSong.range}</div>
-        <div>startingPitch: {currentSong.startingPitch}</div>
-        <div>scale: {currentSong.scale}</div>
-        <div>formAnalysis: {currentSong.formAnalysis}</div>
-        <div>rhythmSet: {currentSong.rhythmSet}</div>
-        <div>tonalCenter: {currentSong.tonalCenter}</div>
-        <div>formType: {currentSong.formType}</div>
+        <SongProperty name={"gradeFloor"} value={currentSong.gradeFloor} editable={this.state.editable}/>
+        <SongProperty name={"gradeCeil"} value={currentSong.gradeCeil} editable={this.state.editable}/>
+        <SongProperty name={"toneSet"} value={currentSong.toneSet} editable={this.state.editable}/>
+        <SongProperty name={"range"} value={currentSong.range} editable={this.state.editable}/>
+        <SongProperty name={"startingPitch"} value={currentSong.startingPitch} editable={this.state.editable}/>
+        <SongProperty name={"scale"} value={currentSong.scale} editable={this.state.editable}/>
+        <SongProperty name={"formAnalysis"} value={currentSong.formAnalysis} editable={this.state.editable}/>
+        <SongProperty name={"rhythmSet"} value={currentSong.rhythmSet} editable={this.state.editable}/>
+        <SongProperty name={"tonalCenter"} value={currentSong.tonalCenter} editable={this.state.editable}/>
+        <SongProperty name={"formType"} value={currentSong.formType} editable={this.state.editable}/>
         <h5>Other information</h5>
-        <div>informantPerformer: {currentSong.informantPerformer}</div>
-        <div>origin: {currentSong.origin}</div>
-        <div>region: {currentSong.region}</div>
-        <div>songTypes: {currentSong.songTypes}</div>
-        <div>source: {currentSong.source}</div>
-        <div>state: {currentSong.state}</div>
-        <div>subSubject: {currentSong.subSubject}</div>
-        <div>subjects: {currentSong.subjects}</div>
+        <SongProperty name={"informantPerformer"} value={currentSong.informantPerformer} editable={this.state.editable}/>
+        <SongProperty name={"origin"} value={currentSong.origin} editable={this.state.editable}/>
+        <SongProperty name={"region"} value={currentSong.region} editable={this.state.editable}/>
+        <SongProperty name={"songTypes"} value={currentSong.songTypes} editable={this.state.editable}/>
+        <SongProperty name={"source"} value={currentSong.source} editable={this.state.editable}/>
+        <SongProperty name={"state"} value={currentSong.state} editable={this.state.editable}/>
+        <SongProperty name={"subSubject"} value={currentSong.subSubject} editable={this.state.editable}/>
+        <SongProperty name={"subjects"} value={currentSong.subjects} editable={this.state.editable}/>
       </div>
     )
   }
 });
+
+// Make the change to Firebase, not this.state.items
+// this.firebaseRefs['items'].push({
+//   text: this.state.text
+// })
 
 var SongListItem = React.createClass({
   render: function() {
@@ -66,17 +91,22 @@ var SongList = React.createClass({
 });
 
 var SongApp = React.createClass({
+  mixins: [ReactFireMixin],
   getInitialState: function() {
     return {items: [], text: ''};
   },
   componentWillMount: function() {
-    this.firebaseRef = new Firebase("https://songdatabase.firebaseio.com/songList/");
-    this.firebaseRef.on("child_added", function(dataSnapshot) {
-      this.state.items.push(dataSnapshot.val());
-      this.setState({
-        items: this.state.items
-      });
-    }.bind(this));
+    this.bindAsArray(new Firebase('https://songdatabase.firebaseio.com/songList/'), 'items')
+    // this.firebaseRef = new Firebase("https://songdatabase.firebaseio.com/songList/");
+    // this.firebaseRef.on("child_added", function(dataSnapshot) {
+    //   this.state.items.push(dataSnapshot.val());
+    //   this.setState({
+    //     items: this.state.items
+    //   });
+    // }.bind(this));
+  },
+  componentWillUnmount: function() {
+    this.firebaseRef.off();
   },
   onChange: function(e) {
     this.setState({text: e.target.value});
@@ -95,7 +125,7 @@ var SongApp = React.createClass({
 
     return (
       <div>
-        <Song currentSong={currentSong} />
+        <Song currentSong={currentSong} firebaseRefs={this.firebaseRefs} />
         <h3>Songs</h3>
         <SongList items={this.state.items} />
         <form onSubmit={this.handleSubmit}>
